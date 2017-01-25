8d7c82f596963bc7ef71a4fb34675bdcde899d49
Bas Smulders <bas.smulders@gmail.com>
2017-01-25 12:38:10 +0100

    Add method doStuff

--- App.java
+++ App.java
@@ -1,5 +1,9 @@
 public class App {
 	public static void main(String args[]) {
 		System.out.println("App.main 1");
+		doStuff();
+	}
+	private static void doStuff() {
+		System.out.println("App.doStuff");
 	}
 }
