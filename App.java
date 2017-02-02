8f9555d2449be6527631566397081eb11756deab
Bas Smulders <bas.smulders@gmail.com>
2017-02-02 11:59:23 +0100

    Add featureZ

--- App.java
+++ App.java
@@ -1,9 +1,13 @@
 public class App {
 	public static void main(String args[]) {
 		System.out.println("App.main");
-		doStuff();
+		//doStuff();
 	}
-	private static void doStuff() {
-		System.out.println("App.doStuff");
+	private static void doStuff(String caller) {
+		System.out.println("App.doStuff: " + caller);
+	}
+	
+	private void featureZ() {
+		doStuff("featureZ");
 	}
 }
