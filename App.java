6f8e8bd8820e9442b2acf4045f40c9ffc3744b76
Bas Smulders <bas.smulders@gmail.com>
2017-02-02 12:51:16 +0100

    Add featureY

--- App.java
+++ App.java
@@ -1,9 +1,13 @@
 public class App {
 	public static void main(String args[]) {
 		System.out.println("App.main");
-		doStuff();
+		doStuff("main");
 	}
-	private static void doStuff() {
-		System.out.println("App.doStuff");
+	private static void doStuff(String caller) {
+		System.out.println("App.doStuff called by: " + caller);
+	}
+	
+	private void featureY() {
+		doStuff("featureY");
 	}
 }
